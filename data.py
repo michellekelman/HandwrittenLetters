dada4e04183bf61fa89b0b0d0eabec0b9fe64440
Michelle Kelman <78247410+michellekelman@users.noreply.github.com>
2023-04-11 12:36:15 -0500

    data processing

---
+++ data.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+if __name__ == '__main__':
+    # loading data
+    train = np.genfromtxt('data/emnist-balanced-train.csv', missing_values=0, skip_header=0, delimiter=',', dtype=int)
+    test = np.genfromtxt('data/emnist-balanced-test.csv', missing_values=0, skip_header=0, delimiter=',', dtype=int)
+    print("Number of examples in train:", len(train))
+    print("Number of examples in test:", len(test))
+
+    # splitting data
+    ytrn = train[:, 0]
+    Xtrn = train[:, 1:]
+    ytst = test[:, 0]
+    Xtst = test[:, 1:]
+    print("Number of examples in y train:", len(ytrn))
+    print("Number of examples in X train:", len(Xtrn))
+    print("Number of examples in y test:", len(ytst))
+    print("Number of examples in X test:", len(Xtst))
+
+    # saving data as np arrays for future use
+    np.save('numpy/ytrn.npy', ytrn)
+    np.save('numpy/Xtrn.npy', Xtrn)
+    np.save('numpy/ytst.npy', ytst)
+    np.save('numpy/Xtst.npy', Xtst)
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import pandas as pd
 import numpy as np
-# from sklearn import _____
+from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import pickle
 
 if __name__ == '__main__':
     # loading data
-    train = pd.read_csv('data/emnist-balanced-train.csv')
-    test = pd.read_csv('data/emnist-balanced-test.csv')
-    print("Number of examples in train:", len(train))
-    print("Number of examples in test:", len(test))
+    ytrn = np.load('numpy/ytrn.npy')
+    Xtrn = np.load('numpy/Xtrn.npy')
+    ytst = np.load('numpy/ytst.npy')
+    Xtst = np.load('numpy/Xtst.npy')
+    print("Data loaded successfully")
 
     # train and test 2 models with our own algorithm implementation
     # # # confusion matrices
@@ -19,6 +19,12 @@ if __name__ == '__main__':
     # # # area under ROC curvegit
 
     # train and test 2 models with scikit-learn mirroring our algorithms
+    relu = MLPClassifier().fit(Xtrn, ytrn)
+    print("Train successful")
+    ypred = relu.predict(Xtst)
+    print("Predict successful")
+    accuracy = relu.score(Xtst, ytst)
+    print(accuracy)
     # # # confusion matrices
     # # # accuracy
     # # # TPR and FPR
