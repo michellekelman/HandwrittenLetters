a6a9d2c7f02eb11165694434084a2e4e97ac3e72
Alan_Wang <945280031@qq.com>
2023-04-24 21:47:28 -0500

    make a PCA class

--- PCA.py
+++ PCA.py
@@ -1,38 +1,44 @@
+import matplotlib as plt
 import numpy as np
 import plotly.express as px
 from tensorflow.python.ops.numpy_ops import np_config
 np_config.enable_numpy_behavior()
-import plotly.express as px
-import matplotlib as plt
 
-def Pca(X, y, num_components):
-    
-    # move the center of the data to the origin
-    X_meaned = X - np.mean(X, axis=0)
 
-    # get the covariance matrix
-    cov_mat = np.cov(X_meaned, rowvar=False)
+class PCA():
 
-    # get the eigen_values, eigen_vectors from the matrix
-    eigen_values, eigen_vectors = np.linalg.eigh(cov_mat)
+    def Pca(self, X, y, num_components):
 
-    # sort the values, pick the top k (k is the dimension, 2 or 3)
-    sorted_index = np.argsort(eigen_values)[::-1]
-    sorted_eigenvalue = eigen_values[sorted_index]
-    sorted_eigenvectors = eigen_vectors[:, sorted_index]
+        # move the center of the data to the origin
+        X_meaned = X - np.mean(X, axis=0)
 
-    eigenvector_subset = sorted_eigenvectors[:, 0:num_components]
+        # get the covariance matrix
+        cov_mat = np.cov(X_meaned, rowvar=False)
 
-    # dot product of the vector and the recentered data (project the data to lower dimension)
-    X_reduced = np.dot(eigenvector_subset.transpose(),
-                       X_meaned.transpose()).transpose()
-    if num_components == 3: 
-      fig = px.scatter_3d(X_reduced, x=0, y=1,
-                          color=y[0], ) # 3d plot
-      fig.update_traces(marker=dict(size=2,
-                                    opacity=0.5,
-                                    ),
-                        selector=dict(mode='markers'))
-      fig.show()
-    elif num_components == 2:
-      plt.scatter(X_reduced[:,0], X_reduced[:,1], c=y[0]) # 2d plot
+        # get the eigen_values, eigen_vectors from the matrix
+        eigen_values, eigen_vectors = np.linalg.eigh(cov_mat)
+
+        # sort the values, pick the top k (k is the dimension, 2 or 3)
+        sorted_index = np.argsort(eigen_values)[::-1]
+        sorted_eigenvalue = eigen_values[sorted_index]
+        sorted_eigenvectors = eigen_vectors[:, sorted_index]
+
+        eigenvector_subset = sorted_eigenvectors[:, 0:num_components]
+
+        # dot product of the vector and the recentered data (project the data to lower dimension)
+        X_reduced = np.dot(eigenvector_subset.transpose(),
+                           X_meaned.transpose()).transpose()
+        return X_reduced
+
+    def plot_3D(self, X, y):
+        fig = px.scatter_3d(X, x=0, y=1,
+                            color=y[0], )  # 3d plot
+        fig.update_traces(marker=dict(size=2,
+                                      opacity=0.5,
+                                      ),
+                          selector=dict(mode='markers'))
+        return fig
+
+    def plot_2D(self, X, y):
+        fig = plt.scatter(X[:, 0], X[:, 1], c=y[0])  # 2d plot
+        return fig
